6e12ad50db34c4dd401b1754410b3a3362c9e711
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-05-21 13:13:18 +0300

    Changed migrations. Because the relationship of many-to-many to one-to-many in table roles has been changed.

--- database/migrations/2019_05_21_100521_create_users_table.php
+++ database/migrations/2019_05_21_100521_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration {
 			$table->dateTime('email_verified_at')->nullable();
 			$table->string('password');
 			$table->string('remember_token', 100)->nullable();
+			$table->integer('role_id')->unsigned()->default(1)->index('FK_users_roles');
 			$table->timestamps();
 		});
 	}
