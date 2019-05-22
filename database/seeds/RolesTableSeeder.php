7b6ee6ef46d398bd3263c939b740269f32269f0c
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-05-22 09:02:35 +0300

    Added Users, Roles seeds and factories

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->delete();
+
+        $roles = [
+            ['id' => 1, 'name' => 'registered'],
+            ['id' => 2, 'name' => 'admin'],
+			['id' => 3, 'name' => 'doctor'],
+        ];
+		
+		Role::insert($roles);
+    }
+}
