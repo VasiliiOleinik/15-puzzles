4ddb7801843eca9c13a110912398c9fc4a223414
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-05-30 10:03:06 +0300

    Created backend.js, backend.css. Backend scripts and styles will be here.

---
+++ public/js/backend.js
@@ -0,0 +1,107 @@
+/* 15 pazzles backend*/
+
+document.addEventListener("DOMContentLoaded", function (event) {
+            
+    // Add _active class to the current piece (highlight it)
+    var div =$("#pieces");
+    var pieces = document.getElementsByName("piece");
+    for (var i = 0; i < pieces.length; i++) {
+        pieces[i].addEventListener("click", function () {
+            if (!$(this).hasClass('_active')) {
+                $(this).addClass('_active')
+            } else {
+                $(this).removeClass('_active')
+            }
+        });
+    }
+
+    $(".piece").bind("click", function() {
+
+        //get active pieces
+        var _active_pieces_id = [];
+        $(".piece").each(function(){
+            if($(this).hasClass('_active')){
+                piece_id = $(this).attr('piece-id');                        
+                _active_pieces_id.push(piece_id);                        
+            }
+        });
+
+        //get content of pieces
+        $.ajax({
+            type: "POST",
+            url: "/pieces_content",
+            data: {
+                        "_token": $('meta[name="csrf-token"]').attr('content'),
+                    _active_pieces_id : _active_pieces_id
+                    },
+            complete: function(result){
+                    
+                if(result.responseText.length == 0){
+                    $('#understanding_the_15').html("");
+                    protocolsContentAjax(null);
+                }else{
+                        
+                    var json_result =  JSON.parse(result.responseText);
+                        
+                    var html = "";
+
+                    for(i = 0; i < json_result.length; i++){
+                        html += "<h4>" + json_result[i]['name'] + "</h4>";
+                        html += json_result[i]['content'];
+                        html += "<br><br>";
+                    }
+                    $('#understanding_the_15').html(html);
+
+                    protocolsContentAjax(_active_pieces_id);
+                }
+            }
+        });
+    });
+
+    function protocolsContentAjax(_active_pieces_id){
+
+        /*if(!_active_pieces_id){
+            $('#protocols_ul').html("");
+        }else*/
+        {
+            //console.log("active pieces: ", _active_pieces_id);
+
+
+            //get content of protocols
+            $.ajax({
+                type: "POST",
+                url: "/protocols_content",
+                data: {
+                    "_token": $('meta[name="csrf-token"]').attr('content'),
+                    _active_pieces_id : _active_pieces_id
+                },
+                complete: function(result){
+                    console.log(result.responseText);
+
+                            
+                    if(result.responseText.length == 0){                            
+                            $('#protocols_ul').html("");
+                    }else{
+                            
+                    var json_result =  JSON.parse(result.responseText);
+                            
+                        var html = "";
+                               
+                        for(i = 0; i < json_result.length; i++){
+                            html += "<li class='list-group-item p-0'>" + json_result[i]['name'] + "</li>";
+                            /*html += json_result[i]['content'];
+                            html += "<br><br>";*/
+                        }
+
+                        $('#protocols_ul').html(html);
+                    }
+                },
+                error:function(err){
+                    console.log("error");
+                }
+            });
+        }
+    }
+});
+
+/**/
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,35 +7,19 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>{{ config('app.name', 'Laravel') }}</title>    
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/backend.css') }}" rel="stylesheet">
 
-    <style>
-    /* Style the buttons */
-    .btn {
-      border: none;
-      outline: none;
-      padding: 10px 16px;
-      background-color: #f1f1f1;
-      cursor: pointer;
-      font-size: 18px;
-    }
-
-    /* Style the active class, and buttons on mouse-over */
-    ._active, .piece:hover {
-      background-color: #66666633;
-      color: white;
-    }
-    </style>
 </head>
 <body>
     <div id="app">
@@ -139,115 +123,10 @@
             @yield('content')
         </main>
     </div>
-    <script>
-        /* 15 pazzles backend*/
 
-        document.addEventListener("DOMContentLoaded", function (event) {
+    <!-- Scripts -->
 
-            // Add _active class to the current piece (highlight it)
-            var div =$("#pieces");
-            var pieces = document.getElementsByName("piece");
-            for (var i = 0; i < pieces.length; i++) {
-                pieces[i].addEventListener("click", function () {
-                    if (!$(this).hasClass('_active')) {
-                        $(this).addClass('_active')
-                    } else {
-                        $(this).removeClass('_active')
-                    }
-                });
-            }
-
-            $(".piece").bind("click", function() {
-
-                //get active pieces
-                var _active_pieces_id = [];
-                $(".piece").each(function(){
-                    if($(this).hasClass('_active')){
-                        piece_id = $(this).attr('piece-id');                        
-                        _active_pieces_id.push(piece_id);                        
-                    }
-                });
-
-                //get content of pieces
-                $.ajax({
-                    type: "POST",
-                    url: "/pieces_content",
-                    data: {
-                            "_token": "{{ csrf_token() }}",
-                            _active_pieces_id : _active_pieces_id
-                            },
-                    complete: function(result){
-                    
-                        if(result.responseText.length == 0){
-                            $('#understanding_the_15').html("");
-                            protocolsContentAjax(null);
-                        }else{
-                        
-                            var json_result =  JSON.parse(result.responseText);
-                        
-                            var html = "";
-
-                            for(i = 0; i < json_result.length; i++){
-                                html += "<h4>" + json_result[i]['name'] + "</h4>";
-                                html += json_result[i]['content'];
-                                html += "<br><br>";
-                            }
-                            $('#understanding_the_15').html(html);
-
-                            protocolsContentAjax(_active_pieces_id);
-                        }
-                    }
-                });
-            });
-
-            function protocolsContentAjax(_active_pieces_id){
-
-                /*if(!_active_pieces_id){
-                    $('#protocols_ul').html("");
-                }else*/
-                {
-                    //console.log("active pieces: ", _active_pieces_id);
-
-
-                    //get content of protocols
-                    $.ajax({
-                        type: "POST",
-                        url: "/protocols_content",
-                        data: {
-                            "_token": "{{ csrf_token() }}",
-                            _active_pieces_id : _active_pieces_id
-                        },
-                        complete: function(result){
-                            console.log(result.responseText);
-
-                            
-                            if(result.responseText.length == 0){                            
-                                 $('#protocols_ul').html("");
-                            }else{
-                            
-                            var json_result =  JSON.parse(result.responseText);
-                            
-                                var html = "";
-                               
-                                for(i = 0; i < json_result.length; i++){
-                                    html += "<li class='list-group-item p-0'>" + json_result[i]['name'] + "</li>";
-                                    /*html += json_result[i]['content'];
-                                    html += "<br><br>";*/
-                                }
-
-                                $('#protocols_ul').html(html);
-                            }
-                        },
-                        error:function(err){
-                            console.log("error");
-                        }
-                    });
-                }
-            }
-        });
-
-        /**/
-
-    </script>
+    <script src="{{ asset('js/backend.js') }}" defer></script>
+    
 </body>
 </html>
