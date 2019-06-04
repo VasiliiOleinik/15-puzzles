31693c55558a15aa2f6b4e27a01f75f42fc0b221
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-06-04 14:26:04 +0300

    Grouping code in backend.js

--- public/js/backend.js
+++ public/js/backend.js
@@ -3,6 +3,9 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
+    /* ------------------ */
+    /*     VARIABLES      */
+    /* ------------------ */
 
     var protocols_ajax;
     var remedies_ajax;
@@ -10,7 +13,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     var tags_pieces = "", tags_diseases = "", tags_protocols = "", tags_remedies = "", tags_markers = "";
 
-    //$(".tm-input").tagsManager();
+    /* ------------------ */
+    /* ------------------ */
+
+
+
+
+    /* ------------------ */
+    /*     LISTENERS      */
+    /* ------------------ */
 
     // Add _active class to the current piece and disease (highlight it)
     var div =$("#pieces_and_diseases");
@@ -34,6 +45,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
         });
     }
+
+    /* ------------------ */
+    /* ------------------ */
+
+
+
+
+    /* --------------------------- */
+    /*   CLICKS ON ITEMS IN TABS   */
+    /* --------------------------- */
 
     $(".piece").bind("click", function() {
 
@@ -125,51 +146,153 @@ document.addEventListener("DOMContentLoaded", function (event) {
         }
 
     });    
+    
+
+    //click on protocol item
+    $('#protocols').delegate("li", "click", function () {
+
+        var _active_pieces_id = getActivePiecesId();
+        if (_active_pieces_id.length == 0) {
+
+            var protocol_id = $(this).attr('obj-id');
+            protocolPiecesAjax(protocol_id);
+        }
+
+        $('#protocols li').removeClass('active');
+        $(this).addClass('active');
+        var id = $(this).attr('obj-id');
+        var table = "protocols";
+        DetailsAjax(id, table);
+        //console.log($(this).attr('obj-id'));
+    });
+
+    //click on remedy item
+    $('#remedies').delegate("li", "click", function () {
+
+        $('#remedies_ajax_container li').removeClass('active');
+        $(this).addClass('active');
+        var id = $(this).attr('obj-id');
+        var table = "remedies";
+        DetailsAjax(id, table);
+        console.log($(this).attr('obj-id'));
+    });
+
+    $('#markers').delegate("li", "click", function () {
+
+        $('#markers_ajax_container li').removeClass('active');
+        $(this).addClass('active');
+        var id = $(this).attr('obj-id');
+        var table = "markers";
+        DetailsAjax(id, table);
+        console.log($(this).attr('obj-id'));
+    });
+
+    /* ------------------ */
+    /* ------------------ */
+
+
+
+
+    /* ------------------ */
+    /*   CLICKS ON TABS   */
+    /* ------------------ */
+
+    //click on protocol tab    
+    $('#tab-protocols').delegate("a", "click", function () {
+
+        $('.details_ajax_container').html('');     
+        $('#protocols_ajax_container').html('Loading..');
+        
+        //get active pieces
+        var _active_pieces_id = getActivePiecesId();
+        //get active diseases
+        var _active_diseases_id = getActiveDiseasesId();
+
+        protocolsContentAjax(_active_pieces_id, _active_diseases_id);
+    });
+
+    //click on remedies tab
+    $('#tab-remedies').delegate("a", "click", function () {
+
+        $('.details_ajax_container').html('');     
+        $('#remedies_ajax_container').html('Loading..');
+        
+        //get active pieces
+        var _active_pieces_id = getActivePiecesId();
+        //get active diseases
+        var _active_diseases_id = getActiveDiseasesId();
+
+        remediesContentAjax(_active_pieces_id, _active_diseases_id);
+    });
+
+    //click on markers tab
+    $('#tab-markers').delegate("a", "click", function () {
+
+        $('.details_ajax_container').html('');     
+        $('#markers_ajax_container').html('Loading..');
+
+        //get active pieces
+        var _active_pieces_id = getActivePiecesId();
+        //get active diseases
+        var _active_diseases_id = getActiveDiseasesId();
+
+        markersContentAjax(_active_pieces_id, _active_diseases_id);
+    });
+
+    /* ------------------ */
+    /* ------------------ */
+
+
+
+    
+    /* ------------------ */
+    /*   AJAX FUNCTIONS   */
+    /* ------------------ */
 
     function protocolsContentAjax(_active_pieces_id, _active_diseases_id) {
-       
+
         try {
             protocols_ajax.abort();
         }
         catch{
-        
+
         }
-   
+
         $('#protocols_ajax_container').html('Loading..');
-        
-            //get content of protocols
-            protocols_ajax = $.ajax({
-                type: "POST",
-                url: "/protocols_content",
-                data: {
-                    "_token": $('meta[name="csrf-token"]').attr('content'),
-                    _active_pieces_id: _active_pieces_id,
-                    _active_diseases_id: _active_diseases_id,
-                },
-                complete: function (result) {
-                    //console.log("protocols: "+result.responseText);
+
+        //get content of protocols
+        protocols_ajax = $.ajax({
+            type: "POST",
+            url: "/protocols_content",
+            data: {
+                "_token": $('meta[name="csrf-token"]').attr('content'),
+                _active_pieces_id: _active_pieces_id,
+                _active_diseases_id: _active_diseases_id,
+            },
+            complete: function (result) {
+                //console.log("protocols: "+result.responseText);
 
 
-                    if (result.responseText.length == 0) {
-                        $('#protocols_ajax_container').html("");
-                    } else {
+                if (result.responseText.length == 0) {
+                    $('#protocols_ajax_container').html("");
+                } else {
 
-                        var json_result = JSON.parse(result.responseText);
+                    var json_result = JSON.parse(result.responseText);
 
-                        var html = "";
+                    var html = "";
 
-                        for (i = 0; i < json_result.length; i++) {
-                            html += "<li class='list-group-item list-group-item-action p-0' obj-id='" + json_result[i]['id'] + "'>" + json_result[i]['name'] + "</li>";
-                        }
-
-                        $('#protocols_ajax_container').html(html);
+                    for (i = 0; i < json_result.length; i++) {
+                        html += "<li class='list-group-item list-group-item-action p-0' obj-id='" + json_result[i]['id'] + "'>" + json_result[i]['name'] + "</li>";
                     }
-                },
-                error: function (err) {
-                    //console.log("protocols ajax error: " + err.responseText);
+
+                    $('#protocols_ajax_container').html(html);
                 }
-            });
-        
+            },
+            error: function (err) {
+                //console.log("protocols ajax error: " + err.responseText);
+            }
+        });
+
     }
 
     function remediesContentAjax(_active_pieces_id, _active_diseases_id) {
@@ -213,7 +336,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             error: function (err) {
                 console.log("remedies ajax error:" + err.responseText);
             }
-        });     
+        });
     }
 
     function markersContentAjax(_active_pieces_id, _active_diseases_id) {
@@ -262,39 +385,39 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     function DetailsAjax(id, table) {
-        
+
         //get details
         $.ajax({
-                type: "POST",
-                url: "/details_content",
-                data: {
-                        "_token": $('meta[name="csrf-token"]').attr('content'),
-                    id: id,
-                    table: table
-                },
-                complete: function (result) {
-                        //console.log("details: " + result.responseText);
-            
-                
-                            if (result.responseText.length == 0) {
-                                    $('#'+table+' .details_ajax_container').html("");
-                                } else {
-                
-                                    var json_result = JSON.parse(result.responseText);
-                
-                                    var html = "";
-                
-                                    html += json_result['content'];
-                                html += "<br><br>";
-                
-                                $('#'+table+' .details_ajax_container').html(html);
-                            }
-                    },
-                error: function (err) {
-                    console.log("protocols ajax error");
-                }
+            type: "POST",
+            url: "/details_content",
+            data: {
+                "_token": $('meta[name="csrf-token"]').attr('content'),
+                id: id,
+                table: table
+            },
+            complete: function (result) {
+                //console.log("details: " + result.responseText);
 
-        });           
+
+                if (result.responseText.length == 0) {
+                    $('#' + table + ' .details_ajax_container').html("");
+                } else {
+
+                    var json_result = JSON.parse(result.responseText);
+
+                    var html = "";
+
+                    html += json_result['content'];
+                    html += "<br><br>";
+
+                    $('#' + table + ' .details_ajax_container').html(html);
+                }
+            },
+            error: function (err) {
+                console.log("protocols ajax error");
+            }
+
+        });
     }
 
     function protocolPiecesAjax(protocol_id) {
@@ -307,7 +430,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             },
             complete: function (result) {
 
-                
+
                 /*
                 if (result.responseText.length != 0) {
                     console.log("protocol_pieces: " + result.responseText);
@@ -329,100 +452,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     });
                 }
                 */
-             
+
             },
             error: function (err) {
                 console.log("protocol_pieces ajax error");
             }
 
-        });     
+        });
     }
 
-    //click on protocol item
-    $('#protocols').delegate("li", "click", function () {
-
-        var _active_pieces_id = getActivePiecesId();
-        if (_active_pieces_id.length == 0) {
-
-            var protocol_id = $(this).attr('obj-id');
-            protocolPiecesAjax(protocol_id);
-        }
-
-        $('#protocols li').removeClass('active');
-        $(this).addClass('active');
-        var id = $(this).attr('obj-id');
-        var table = "protocols";
-        DetailsAjax(id, table);
-        //console.log($(this).attr('obj-id'));
-    });
-
-    //click on remedy item
-    $('#remedies').delegate("li", "click", function () {
-
-        $('#remedies_ajax_container li').removeClass('active');
-        $(this).addClass('active');
-        var id = $(this).attr('obj-id');
-        var table = "remedies";
-        DetailsAjax(id, table);
-        console.log($(this).attr('obj-id'));
-    });
-
-    $('#markers').delegate("li", "click", function () {
-
-        $('#markers_ajax_container li').removeClass('active');
-        $(this).addClass('active');
-        var id = $(this).attr('obj-id');
-        var table = "markers";
-        DetailsAjax(id, table);
-        console.log($(this).attr('obj-id'));
-    });
+    /* ------------------ */
+    /* ------------------ */
 
 
 
-    //click on protocol tab    
-    $('#tab-protocols').delegate("a", "click", function () {
 
-        $('.details_ajax_container').html('');     
-        $('#protocols_ajax_container').html('Loading..');
-        
-        //get active pieces
-        var _active_pieces_id = getActivePiecesId();
-        //get active diseases
-        var _active_diseases_id = getActiveDiseasesId();
-
-        protocolsContentAjax(_active_pieces_id, _active_diseases_id);
-    });
-
-    //click on remedies tab
-    $('#tab-remedies').delegate("a", "click", function () {
-
-        $('.details_ajax_container').html('');     
-        $('#remedies_ajax_container').html('Loading..');
-        
-        //get active pieces
-        var _active_pieces_id = getActivePiecesId();
-        //get active diseases
-        var _active_diseases_id = getActiveDiseasesId();
-
-        remediesContentAjax(_active_pieces_id, _active_diseases_id);
-    });
-
-    //click on markers tab
-    $('#tab-markers').delegate("a", "click", function () {
-
-        $('.details_ajax_container').html('');     
-        $('#markers_ajax_container').html('Loading..');
-
-        //get active pieces
-        var _active_pieces_id = getActivePiecesId();
-        //get active diseases
-        var _active_diseases_id = getActiveDiseasesId();
-
-        markersContentAjax(_active_pieces_id, _active_diseases_id);
-    });
-
-    
-
+    /* ------------------ */
+    /*      FUNCTIONS     */
+    /* ------------------ */
 
     function getActivePiecesId() {
         
@@ -478,6 +525,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         return _active_diseases_id;
     }
+
+     /* ------------------ */
+    /* ------------------ */
 });
 
 /**/
