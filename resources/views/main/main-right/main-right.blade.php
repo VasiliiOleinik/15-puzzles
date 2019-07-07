a31633a0e650b223699f49dc2006ae3f552d99b1
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-07-07 13:58:07 +0300

    Added preview poster on video on main page.

--- resources/views/main/main-right/main-right.blade.php
+++ resources/views/main/main-right/main-right.blade.php
@@ -1,7 +1,7 @@
 <div class="main__right">
   <div class="main__video">
-    <div class="main__video-overlay"><img class="main__video-logo" src="img/svg/logo.svg"></div>
-    <video>
+    <div class="main__video-overlay"><!--<img class="main__video-logo" src="img/svg/logo.svg">--></div>
+    <video poster="img/Video.png">
       <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
       <source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
     </video>
