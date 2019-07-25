eabc7518760876dff9e8335451c63226a67d3f04
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-07-25 15:37:44 +0300

    Fixed bug with avatar on admin/users page.

--- app/Http/Admin/Users.php
+++ app/Http/Admin/Users.php
@@ -100,7 +100,6 @@ class Users extends Section implements Initializable
                 AdminColumn::text('last_name')->setLabel('отчество'),
                 AdminColumn::text('email')->setLabel('почта'),
                 AdminColumn::text('birthday')->setLabel('дата рождения'),
-                AdminColumn::image('img')->thumbnail('original'),
             );
         $display
             //поля поиска
