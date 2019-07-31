9ba5e0765ad12115ee33df9f3f8c3ffa71f38478
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-07-31 14:27:09 +0300

    Fixed issue with cache remember factors with localization.

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use App\Models\Factor\FactorLanguage;
 
 class AboutController extends Controller
 {
@@ -15,10 +16,10 @@ class AboutController extends Controller
     public function index()
     {
         $factors = Cache::remember(
-            'factor',
+            'factor_'.app()->getLocale(),
             now()->addDay(1),
             function(){
-                return Factor::with('type')->get();
+                return FactorLanguage::with('type')->get();
             }
         );
         return view('about.about', compact(['factors']));
