1890592d901e97cc6115d68cb8d2745e3d4723af
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-08-23 07:47:45 +0300

    Refactor: floating logotope fixed, lang css updated.

--- resources/views/layouts/include/css.blade.php
+++ resources/views/layouts/include/css.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/lang.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/libs.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/jquery-ui.min.css') }}">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
@@ -8,7 +9,6 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/main.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/custom.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/lang.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/backend/auth.css') }}">
 @yield('verify-css')
 @yield('main-css')
