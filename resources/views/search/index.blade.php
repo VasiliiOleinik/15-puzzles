69b9c02ba2a40b66b44de54a01e31d5aa495427b
Aleksandr Terekhov <fineworkcom@gmail.com>
2019-09-07 18:38:58 +0300

    Fix: pretty urls in search

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -16,13 +16,19 @@
                 <div class="search__content-items">
                 @foreach($results as $collection)
                   @foreach($collection as $result)
+                    @php
+                      //форматируем
+                      $resultTitle = mb_strtolower($result->title);
+                      $resultTitle = preg_replace('#[[:punct:]]#', '', $resultTitle);
+                      $resultTitle = str_replace(' ', '-', $resultTitle);
+                    @endphp
                     @if($result->getTable() == "article_languages")
-                    <div class="search-item"><a href="{{ url(app()->getLocale().'/news') }}/{{$result->article_id}}"><img class="search-item-img" src="{{$result->article->img }}"></a>
-                      <div class="search-item-detail"><a class="search-item-detail-date" href="{{ url(app()->getLocale().'/news') }}/{{$result->article_id}}">{{$result->article->updated_at->format('d.m.Y')}}</a><a class="search-item-detail-section" href="{{ url(app()->getLocale().'/news') }}/{{$result->article_id}}">@lang('search.label_news')</a></div><a class="search-item-name" href="{{ url(app()->getLocale().'/news') }}/{{$result->article_id}}">{{$result->title }}</a>
+                    <div class="search-item"><a href="{{ url(app()->getLocale().'/news') }}/{{ $resultTitle }}"><img class="search-item-img" src="{{$result->article->img }}"></a>
+                      <div class="search-item-detail"><a class="search-item-detail-date" href="{{ url(app()->getLocale().'/news') }}/{{ $resultTitle }}">{{$result->article->updated_at->format('d.m.Y')}}</a><a class="search-item-detail-section" href="{{ url(app()->getLocale().'/news') }}/{{ $resultTitle }}">@lang('search.label_news')</a></div><a class="search-item-name" href="{{ url(app()->getLocale().'/news') }}/{{ $resultTitle }}">{{$result->title }}</a>
                     </div> 
                     @elseif($result->getTable() == "member_cases")
-                    <div class="search-item"><a href="{{ url(app()->getLocale().'/member_cases') }}/{{$result->id}}"><img class="search-item-img" src="{{$result->img }}"></a>
-                      <div class="search-item-detail"><a class="search-item-detail-date" href="{{ url(app()->getLocale().'/member_cases') }}/{{$result->id}}">{{$result->updated_at->format('d.m.Y')}}</a><a class="search-item-detail-section" href="{{ url(app()->getLocale().'/member_cases') }}/{{$result->id}}">@lang('search.label_member_cases')</a></div><a class="search-item-name" href="{{ url(app()->getLocale().'/member_cases') }}/{{$result->id}}">{{$result->title }}</a>
+                    <div class="search-item"><a href="{{ url(app()->getLocale().'/member_cases') }}/{{ $resultTitle }}"><img class="search-item-img" src="{{$result->img }}"></a>
+                      <div class="search-item-detail"><a class="search-item-detail-date" href="{{ url(app()->getLocale().'/member_cases') }}/{{$result->id}}">{{$result->updated_at->format('d.m.Y')}}</a><a class="search-item-detail-section" href="{{ url(app()->getLocale().'/member_cases') }}/{{ $resultTitle }}">@lang('search.label_member_cases')</a></div><a class="search-item-name" href="{{ url(app()->getLocale().'/member_cases') }}/{{ $resultTitle }}">{{$result->title }}</a>
                     </div> 
                     @endif                  
                   @endforeach
