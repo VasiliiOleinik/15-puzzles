6e3bef5178b6795fc4113d87b7933229ffeb93f6
Oleynik Vlad <oleynikprog@gmail.com>
2019-10-13 20:12:57 +0300

    Add: diagramm

--- app/Http/Controllers/FactorDiagramController.php
+++ app/Http/Controllers/FactorDiagramController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Factor\Factor;
+use App\Models\Factor\FactorLanguage;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class FactorDiagramController extends Controller
@@ -13,6 +16,11 @@ class FactorDiagramController extends Controller
      */
     public function index()
     {
-    return view('factor-diagram.factor-diagram');
+        $factorTypes = Type::with('factors')->get();
+        return view('factor-diagram.factor-diagram',
+            compact(
+                'factorTypes'
+            )
+        );
     }
 }
