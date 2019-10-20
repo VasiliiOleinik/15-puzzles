7aefc19814e13f47c5361f4afd90a7049629d829
Виталий Юрьевич <admin@row-dev.com>
2019-10-20 21:05:33 +0300

    Fix: login and email inputs is disabled + css

--- resources/views/personal-cabinet/personal-left/main-content/profile-labels.blade.php
+++ resources/views/personal-cabinet/personal-left/main-content/profile-labels.blade.php
@@ -1,14 +1,9 @@
                   <div class="profile-labels">
                      <!-- avatar -->
                      <input id="img" type="hidden" name="img">
-                    <!- - ->
                     <!-- nickname -->
                     <div class="label">
-                      <!--
-                      <input name="nickname" type="text">
-                      <label for="nickname">Nickname</label>
-                      -->
-                      <input id="nickname" type="text"class="form-control @error('nickname') is-invalid @enderror" name="nickname" value="{{Auth::user()->nickname}}" required autocomplete="nickname" autofocus>
+                      <input id="nickname" type="text"class="form-control @error('nickname') is-invalid @enderror" name="nickname" value="{{Auth::user()->nickname}}" disabled>
                       <label for="nickname">@lang('personal_cabinet.nickname')</label>
                       @error('nickname')
                         <label class="invalid-feedback" role="alert">
@@ -16,14 +11,9 @@
                         </label>
                       @enderror
                     </div>
-                    <!- - ->
-                    <!-- email -->
                     <div class="label required">
-                      <!--
-                      <input name="email" type="text">
-                      <label for="email">Email<span>*</span></label>
-                      -->
-                      <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{Auth::user()->email}}" required autocomplete="email" autofocus>
+
+                      <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{Auth::user()->email}}" disabled>
                       <label for="email">@lang('personal_cabinet.email')<span>*</span></label>
                       @error('email')
                         <label class="invalid-feedback" role="alert">
@@ -31,13 +21,8 @@
                         </label>
                       @enderror
                     </div>
-                    <!- - ->
-                    <!-- first name -->
                     <div class="label required">
-                      <!--
-                      <input name="name" type="text">
-                      <label for="name">Name<span>*</span></label>
-                      -->
+
                       <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{Auth::user()->first_name}}" autocomplete="first_name" autofocus>
                       <label for="name">@lang('personal_cabinet.name')<span>*</span></label>
                       @error('first_name')
@@ -46,13 +31,8 @@
                          </span>
                       @enderror
                     </div>
-                    <!- - ->
-                    <!-- last name -->
+
                     <div class="label required">
-                      <!--
-                      <input name="last_name" type="text">
-                      <label for="last_name">Last Name<span>*</span></label>
-                      -->
                       <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{Auth::user()->last_name}}" autocomplete="last_name" autofocus>
                       <label for="last_name">@lang('personal_cabinet.last_name')<span>*</span></label>
                       @error('last_name')
@@ -61,13 +41,8 @@
                         </span>
                       @enderror
                     </div>
-                    <!- - ->
-                    <!-- middle name -->
+
                     <div class="label required">
-                      <!--
-                      <input name="middle_name" type="text">
-                      <label for="middle_name">Middle Name<span>*</span></label>
-                      -->
                       <input id="middle_name" type="text" class="form-control @error('middle_name') is-invalid @enderror" name="middle_name" value="{{Auth::user()->middle_name}}" autocomplete="middle_name" autofocus>
                       <label for="middle_name">@lang('personal_cabinet.middle_name')<span>*</span></label>
                       @error('middle_name')
@@ -76,13 +51,8 @@
                         </span>
                       @enderror
                     </div>
-                    <!- - ->
-                    <!-- birthday -->
+
                     <div class="label required">
-                      <!--
-                      <input class="date-inp" id="personal-dob" name="dob" type="text">
-                      <label for="dob">Date Of Birth<span>*</span></label>
-                      -->
                       <input class="date-inp" id="birthday" name="birthday" value="{{Auth::user()->birthday}}" type="text">
                       <label for="dob">@lang('personal_cabinet.date_of_birth')<span>*</span></label>
                       @error('birthday')
@@ -91,8 +61,7 @@
                         </span>
                       @enderror
                     </div>
-                    <!- - ->
-                    <!-- password -->
+
                     <div class="label">
                       <input id="password" type="text" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="password" autofocus>
                       <label for="nickname">@lang('personal_cabinet.password')</label>
@@ -101,10 +70,6 @@
                           <strong>{{ $message }}</strong>
                         </label>
                       @enderror
-                      <!--
-                      <input name="password" type="password">
-                      <label for="nickname">Password</label>
-                      -->
                     </div>
                     <!- - ->
                     <div class="message">
