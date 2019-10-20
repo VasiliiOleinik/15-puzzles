0e0e33898c745aebe5415afcaa98fe0bf4f2a817
Efremenko Alex <alexeyefr@gmail.com>
2019-10-20 13:41:11 +0300

    Fix: fix links for news on main page

--- resources/views/main/main-left/home-news.blade.php
+++ resources/views/main/main-left/home-news.blade.php
@@ -4,9 +4,9 @@
     @foreach($newsLatest as $news)
     <div class="home-news__item"><img class="news-image lazy" data-src="{{$news->article->img}}" alt="">
       <div class="news-detail">
-        <a class="news-name" href="{{ url(app()->getLocale().'/news') }}/{{$news->alias}}">{{$news->title}}</a>
-        <a class="news-date" href="{{ url(app()->getLocale().'/news') }}/{{$news->alias}}">{{$news->article->updated_at->format('d.m.Y')}}</a>
-        <a class="news-text " href="{{ url(app()->getLocale().'/news') }}/{{$news->alias}}">{{$news->description}}
+        <a class="news-name" href="{{ url(app()->getLocale().'/news') }}/{{$news->article->alias}}">{{$news->title}}</a>
+        <a class="news-date" href="{{ url(app()->getLocale().'/news') }}/{{$news->article->alias}}">{{$news->article->updated_at->format('d.m.Y')}}</a>
+        <a class="news-text " href="{{ url(app()->getLocale().'/news') }}/{{$news->article->alias}}">{{$news->description}}
           <span class="news-arrow"></span>
         </a>
       </div>
