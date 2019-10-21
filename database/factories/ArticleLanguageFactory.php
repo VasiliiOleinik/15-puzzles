0916a2e4c1c9226e097b39647559b5d8b8f5b29b
Efremenko Alex <alexeyefr@gmail.com>
2019-10-21 10:32:08 +0300

    Fix: fix ArticleLanguageFactory

--- database/factories/ArticleLanguageFactory.php
+++ database/factories/ArticleLanguageFactory.php
@@ -15,21 +15,21 @@ $factory->define(ArticleLanguage::class, function (Faker $faker) {
     $russian = json_decode($jsonString, true);
 
     if( Config::get('app.faker_locale') == "en_US" ){
-        $locale = "eng";        
+        $locale = "eng";
         $content = $faker->realText(600);
-        $tableId = ArticleLanguage::count() + 1;
+        $tableId = ArticleLanguage::withoutGlobalScopes()->count() + 1;
         $title = $faker->realText( rand(15,35) );//Protocol::find($protocol_id)->name;
     }else{
         $locale = "ru";
         $title = $russian["title"][ rand( 0, 21) ]; //$faker->realText(30);
-        $content = $russian["text"][ rand( 0, 21) ];        
-        $tableId = ArticleLanguage::count() + 1 - Article::count();
+        $content = $russian["text"][ rand( 0, 21) ];
+        $tableId = ArticleLanguage::withoutGlobalScopes()->count() + 1 - Article::count();
     }
 
     return [
         'language' => $locale,
         'title' => $title,
-        'content' => $content, 
+        'content' => $content,
         'description' => iconv_substr($content,0,100 , "UTF-8"),
         $tableShort.'_id' => $tableId,
     ];
