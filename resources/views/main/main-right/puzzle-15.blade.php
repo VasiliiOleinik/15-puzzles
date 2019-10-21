c15b4f727215b78fc49d25c2e3191e10b9ed6dc8
Vasilii <goooneoooogle@gmail.com>
2019-10-21 14:25:34 +0300

    Fix scroll on main page and refactoring factors

--- resources/views/main/main-right/puzzle-15.blade.php
+++ resources/views/main/main-right/puzzle-15.blade.php
@@ -1,17 +1,19 @@
 <div class="puzzle-15">
     @foreach($typeFactors as $typeFactor)
-        <div class="wrapp-for_category">
-            <div class="puzzle-15__category">
-                <span></span>{{isset($typeFactor->groupLanguage->name) ? $typeFactor->groupLanguage->name :false}}</div>
-            @foreach ($typeFactor->factors as $factor)
-                <div class="puzzle-15__item-outer" obj-id="{{$factor->id}}">
-                    <div class="puzzle-15__item {{$factor->type->name}}">
-                        <img class="factors-check" src="/img/svg/factors-check.svg" alt="">
-                        <img src="{{$factor->img}}">
-                        <h6 class="puzzle-15__item-title">{{$factor->factorLanguage->name}}</h6>
+        <div class="wrapp-for_category {{isset($typeFactor->groupEng->name) ? $typeFactor->groupEng->name : false}}">
+            <div class="wrapp-for_cat-items">
+                @foreach ($typeFactor->factors as $factor)
+                    <div class="puzzle-15__item-outer" obj-id="{{$factor->id}}">
+                        <div class="puzzle-15__item {{$factor->type->name}}">
+                            <h6 class="puzzle-15__item-title">{{$factor->factorLanguage->name}}</h6>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
+            <div class="category">
+                <span></span>{{isset($typeFactor->groupLanguage->name) ? $typeFactor->groupLanguage->name :false}}
+            </div>
         </div>
     @endforeach
 </div>
+
