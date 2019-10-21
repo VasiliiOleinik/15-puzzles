cc36a0f9f6d728e138030d63eb0b1879ffb5c6af
Vasilii <goooneoooogle@gmail.com>
2019-10-21 17:21:27 +0300

    Fix structure main factors

--- resources/views/main/main-right/puzzle-15.blade.php
+++ resources/views/main/main-right/puzzle-15.blade.php
@@ -1,24 +1,26 @@
 <div class="puzzle-15">
     @foreach($typeFactors as $key => $typeFactor)
         @if(count($typeFactor[0])>0)
-        @foreach($typeFactor[0] as $factors)
-            <div class="wrapp-for_category {{$key}}">
-                <div class="puzzle-15__category">
-                    <span></span>{{$typeFactor['nameTypeLang']}}</div>
-                @foreach ($factors as $factor)
-                    <div class="puzzle-15__item-outer" obj-id="{{$factor->id}}">
-                        <div class="puzzle-15__item {{isset($factor->type->name) ? $factor->type->name : false  }}">
-                            <img class="factors-check" src="/img/svg/factors-check.svg" alt="">
-                            <img src="{{$factor->img}}">
-                            <h6 class="puzzle-15__item-title">{{$factor->factorLanguage->name}}</h6>
-                        </div>
+            @foreach($typeFactor[0] as $factors)
+                <div class="wrapp-for_category {{$key}}">
+                    <div class="wrapp-for_cat-items">
+                        @foreach ($factors as $factor)
+                            <div class="puzzle-15__item-outer" obj-id="{{$factor->id}}">
+                                <div class="puzzle-15__item {{isset($factor->type->name) ? $factor->type->name : false  }}">
+                                    <img class="factors-check" src="/img/svg/factors-check.svg" alt="">
+                                    <img src="{{$factor->img}}">
+                                    <h6 class="puzzle-15__item-title">{{$factor->factorLanguage->name}}</h6>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
-                @endforeach
-            </div>
-        @endforeach
-            @else
+                    <div class="category">
+                        <span></span>{{$typeFactor['nameTypeLang']}}</div>
+                </div>
+            @endforeach
+        @else
             <div class="wrapp-for_category {{$key}}">
-                <div class="puzzle-15__category">
+                <div class="category">
                     <span></span>{{$typeFactor['nameTypeLang']}}</div>
             </div>
         @endif
