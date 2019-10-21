75fcdf85f20d8d6e6c04dc4665e88b3fdb316ad2
Vasilii <goooneoooogle@gmail.com>
2019-10-21 11:33:32 +0300

    formalization after registration page

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,26 +1,22 @@
 @extends('layouts.app')
 @section('verify-css')
-    <link href="{{ asset('css/backend/verify.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/frontend/verify.css') }}" rel="stylesheet">
 @endsection
 @section('content')
 <main>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">@lang('verify.title')</div>
-                    <div class="card-body">
+            <div class="verify-content">
+                    <div class="verify-content-header">@lang('verify.title')</div>
+                    <div class="verify-content-body">
                         @if (session('resent'))
                             <div class="alert alert-success" role="alert">
                                 @lang('verify.notification')
                             </div>
                         @endif
 
-                        @lang('verify.message') <a href="{{ route('verification.resend', app()->getLocale()) }}">@lang('verify.link_title')</a>
+                        @lang('verify.message') <a href="{{ route('verification.resend', app()->getLocale()) }}" class="verify-content-body-link">@lang('verify.link_title')</a>
                     </div>
                 </div>
-            </div>
-        </div>
     </div>
 </main>
 @endsection
