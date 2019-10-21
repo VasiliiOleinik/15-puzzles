54d49158ce0270a61928f9eef972b2da99870aff
Efremenko Alex <alexeyefr@gmail.com>
2019-10-21 10:18:33 +0300

    Fix: fix ProtocolLanguageFactory

--- database/factories/ProtocolLanguageFactory.php
+++ database/factories/ProtocolLanguageFactory.php
@@ -8,6 +8,7 @@ use Faker\Generator as Faker;
 use Faker\Factory as Factory;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 $factory->define(ProtocolLanguage::class, function (Faker $faker){
 
@@ -18,22 +19,22 @@ $factory->define(ProtocolLanguage::class, function (Faker $faker){
     $russian = json_decode($jsonString, true);
 
     if( Config::get('app.faker_locale') == "en_US" ){
-        $locale = "eng";        
+        $locale = "eng";
         $content = $faker->realText(600);
         $tableId = ProtocolLanguage::count() + 1;
         $name = $faker->name."'s "." protocol";//Protocol::find($protocol_id)->name;
     }else{
         $locale = "ru";
         $name = "Протокол ".$faker->name;
-        $content = $russian["text"][ rand( 0, 21) ];        
-        $tableId = ProtocolLanguage::count() + 1 - Protocol::count();
+        $content = $russian["text"][ rand( 0, 21) ];
+        $tableId = ProtocolLanguage::withoutGlobalScopes()->count() + 1 - Protocol::count();
     }
     $evidenceId = Protocol::find($tableId)->evidence_id;
 
     return [
         'language' => $locale,
         'name' => $name,
-        'content' => $content, 
+        'content' => $content,
         'subtitle' => iconv_substr($content,0,100 , "UTF-8"),
         $tableShort.'_id' => $tableId,
         'evidence_id' => $evidenceId,
