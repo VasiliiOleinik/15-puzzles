e2aa8476cb13e638ff5ae6421128d78779a7e7d8
Vasilii <goooneoooogle@gmail.com>
2019-10-21 17:59:53 +0300

    Change struct for right side cideo

--- resources/views/main/main-right/main-right.blade.php
+++ resources/views/main/main-right/main-right.blade.php
@@ -1,6 +1,15 @@
 <div class="main__right">
-  <div class="">
-    {!! $page->page->video !!}
+  <div class="main__video">
+    {{-- {!! $page->page->video !!} --}}
+    <iframe 
+      width="100%"
+      height="100%" 
+      src="https://www.youtube.com/embed/VQerrbCqpIY" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen
+    >
+    </iframe>
   </div>
   @include('main.main-right.puzzle-15')
   <div class="main__right-note">@lang('main.text_puzzles')</div>
