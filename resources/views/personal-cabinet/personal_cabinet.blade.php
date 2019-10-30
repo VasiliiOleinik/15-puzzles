22b731be6a2448ce78d402ef3cf6f318ff64575b
Efremenko Alex <alexeyefr@gmail.com>
2019-10-30 13:54:00 +0200

    Fix: translation for datepicker in personal cabinet
    Took 30 minutes

--- resources/views/personal-cabinet/personal_cabinet.blade.php
+++ resources/views/personal-cabinet/personal_cabinet.blade.php
@@ -7,17 +7,20 @@
 @endsection
 @section('personal_cabinet-css')
     <link href="{{ asset('css/backend/personal_cabinet.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/backend/bootstrap datepicker/bootstrap-datepicker.standalone.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('vendor/laraberg/css/laraberg.css')}}">
 @endsection
 @section('content')
         <main class="main">
           <div class="box">
             @include('personal-cabinet.personal-left.personal-left')
-            @include('personal-cabinet.personal-right.personal-right')              
+            @include('personal-cabinet.personal-right.personal-right')
           </div>
         </main>
 @endsection
 @section('news-js')
 
     <script src="{{ asset('js/backend/personal_cabinet.js') }}" defer></script>
+    <script src="{{ asset('js/backend/bootstrap datepicker/bootstrap-datepicker.min.js') }}" defer></script>
+    <script src="{{ asset('js/backend/bootstrap datepicker/bootstrap-datepicker.ru.js') }}" charset="UTF-8" defer></script>
 @endsection
